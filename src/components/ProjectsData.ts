f84efbe4e1dd2af3dde3b28bf4d1544d700e0f71
Megan <sydiaha.megan@gmail.com>
2024-03-15 15:57:22 -0400

    Changed image path

--- src/components/ProjectsData.ts
+++ src/components/ProjectsData.ts
@@ -14,7 +14,7 @@ const projectsData = [
       "I built this classic dice-rolling game as a means to learn about routing. I used Bootstrap along with some custom colours and fonts to create an eye-catching and retro look and feel. This program was also used as a way to test out deploying a virtual machine via AWS, which made Python the obvious choice of language.",
     technologies: "Python, Bootstrap",
     link: "https://github.com/Megan-Syd/DiceGame.git",
-    image: "src/assets/project_images/dice-game-1.PNG",
+    image: "../assets/project_images/dice-game-1.PNG",
   },
   // Add more projects as needed
 ];
