603cff7d3f9ffaf6c2e8aefa783b33a31b2e7b63
Megan <sydiaha.megan@gmail.com>
2024-03-28 15:40:52 -0400

    edit icon file path

--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -11,7 +11,7 @@ import PhpIcon from "/assets/language_icons/php.svg";
 import PythonIcon from "/assets/language_icons/python.svg";
 import SpringIcon from "/assets/language_icons/spring-icon.svg";
 import TSIcon from "/assets/language_icons/typescript-icon.svg";
-import LaravelIcon from "/assets/language_icons/github.svg";
+import LaravelIcon from "/assets/language_icons/laravel.svg";
 import PostmanIcon from "/assets/language_icons/postman.svg";
 import GithubIcon from "/assets/language_icons/github.svg";
 import ChakraIcon from "/assets/language_icons/chakra.svg";
