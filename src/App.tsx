1fd102de95e9814bf5c0325baf64670f1e3c5714
Megan <sydiaha.megan@gmail.com>
2024-05-02 14:52:57 -0400

    SEO

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,32 @@ import Projects from "./components/Projects";
 import Languages from "./components/Languages";
 import Contact from "./components/Contact";
 import "./App.css";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>Megan Sydiaha | Web Developer</title>
+        <meta
+          name="description"
+          content="Professional portfolio of Megan Sydiaha's web development career."
+        />
+        <meta name="author" content="Megan Sydiaha" />
+        <meta
+          name="keywords"
+          content="web developer, web design, software developer, front-end developer, full stack developer"
+        />
+        <meta property="og:title" content="Megan Sydiaha | Web Developer" />
+        <meta
+          property="og:description"
+          content="Professional portfolio of Megan Sydiaha's web development career."
+        />
+        <meta
+          property="og:url"
+          content="https://megansydiahaportfolio.vercel.app/"
+        />
+      </Helmet>
       <Header />
       <About />
       <hr className="accent-color-1 mx-4" />
