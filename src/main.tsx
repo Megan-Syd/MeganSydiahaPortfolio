fe9f91691b56a726ed5c85c75849e3bf5895419b
Meegun <134974590+Meegun@users.noreply.github.com>
2024-08-09 14:28:26 -0600

    reorders css file imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import "./index.css";
-import "./Fonts.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import "./Fonts.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
