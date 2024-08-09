11132bc0cbed493872ec2f01d6580ef5f9902ae0
Meegun <134974590+Meegun@users.noreply.github.com>
2024-08-09 14:07:44 -0600

    removes and recreates dist files

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import "./index.css";
+import "./Fonts.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
-import "./Fonts.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
